e4d8e5943c01eddd2a23a70733adba4fc482601f
Yung70329360 <104255362+yung70329360@users.noreply.github.com>
2023-01-21 00:41:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import loreal
 import time
-from joblib import Parallel, delayed
+from joblib import Parallel, delayed, cpu_count
 st.image('image.png')
 st.write('Click "Start" to generate votes')
 t = st.empty()
@@ -9,18 +9,19 @@ col1, col2 = st.columns(2)
 with col1:
     start = st.button('Start', key=1)
 
+
 def loop(i):
     try:
         loreal.main()
         t.write(f'第{i}個票數已產生完成')
     except:
-        tt2.write('發生錯誤')
+        tt2.write('發生錯誤, 重新嘗試')
         loop(i)
 
 tt = st.empty()
 tt2 = st.empty()
 
-
+i = 0
 
 if start:
     with col2:
@@ -29,9 +30,10 @@ if start:
         if stop:
             tt.write('正在停止')
             break
-        Parallel(n_jobs=-1)(delayed(loop)(i) for i in range(2500))
+        i += 1
+        loop(1)
     tt.write('已停止')
-      
+    
 
 
 
